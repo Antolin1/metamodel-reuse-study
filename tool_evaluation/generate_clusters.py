7f00d930a06f3fffbd70f9e19277ea99185f2109
alfonsodelavega <alfonso.delavega@unican.es>
2025-08-25 14:08:49 +0200

    Add tool evaluation cluster generation script

--- tool_evaluation/generate_clusters.py
+++ tool_evaluation/generate_clusters.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 #%%
-
 import csv
 import glob
 import random
@@ -11,11 +10,22 @@ from pprint import pprint
 
 from dpu_utils.codeutils.deduplication import DuplicateDetector
 
-TARGET_LABEL = '008'
 FOLDER = 'manualDomains'
 SEED = 123
 
 random.seed(SEED)
+
+#%%
+counts_by_domain = {}
+for file in glob.glob(f"{FOLDER}/*.ecore"):
+    domain = file.split('_')[1]
+    if domain not in counts_by_domain:
+        counts_by_domain[domain] = 0
+    counts_by_domain[domain] += 1
+
+pprint(counts_by_domain)
+
+
 
 #%%
 def get_attribute_values(root, attribute_name):
@@ -27,50 +37,61 @@ def get_attribute_values(root, attribute_name):
 
 
 #%%
-detector = DuplicateDetector(min_num_tokens_per_document=5)
+def get_duplicates(target_label):
+    detector = DuplicateDetector(min_num_tokens_per_document=5)
 
-num_files = 0
-for file in glob.glob(f"{FOLDER}/*.ecore"):
-    domain = file.split('_')[1]
-    if domain == TARGET_LABEL:
-        tree = ET.parse(file)
-        root = tree.getroot()
-        attribute_values = get_attribute_values(root, 'name')
-        detector.add_file(file, attribute_values, language=None)
-        num_files += 1
+    num_files = 0
+    for file in glob.glob(f"{FOLDER}/*.ecore"):
+        domain = file.split('_')[1]
+        if domain == target_label:
+            tree = ET.parse(file)
+            root = tree.getroot()
+            attribute_values = get_attribute_values(root, 'name')
+            detector.add_file(file, attribute_values, language=None)
+            num_files += 1
 
-print(f"Added {num_files} files to the detector")
+    print(f"Added {num_files} files to the detector for label {target_label}")
 
-duplicates = detector.compute_duplicates()
-detector.print_clone_set_stats(duplicates)
+    return detector.compute_duplicates()
+
 
 #%%
-positive_samples = []
-negative_representatives = []
+def generate_datasets():
+    labels = ['008', '005'] # state machines and databases
 
-for group in duplicates:
-    # for positives, all elements in the cluster are needed, the first of the
-    #     random suffle is used as representative
-    positives = list(group)
-    random.shuffle(positives)
-    positive_samples.append(positives)
+    for label in labels:
+        positive_samples = []
+        negative_representatives = []
 
-    # for negatives, a representative is randomly selected
-    negative_representatives.append(random.sample(list(group), 1)[0])
+        for group in get_duplicates(label):
+            # for positives, all elements in the cluster are needed, the first of the
+            #     random suffle is used as representative
+            positives = list(group)
+            random.shuffle(positives)
+            positive_samples.append(positives)
 
-print("Positive")
-pprint(positive_samples)
-with open('positive.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    cont = 0
-    for item in positive_samples:
-        for i in item:
-            writer.writerow([i, cont])
-        cont += 1
+            # for negatives, a representative is randomly selected
+            negative_representatives.append(random.sample(list(group), 1)[0])
 
-print("Negative")
-pprint(negative_representatives)
-with open('negative.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    for item in negative_representatives:
-        writer.writerow([item])
+        print(f"Label {label}:")
+        print("Positive")
+        pprint(positive_samples)
+        with open(f'label_{label}_positive.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            cont = 0
+            for item in positive_samples:
+                for i in item:
+                    writer.writerow([i, cont])
+                cont += 1
+
+        print("Negative")
+        pprint(negative_representatives)
+        with open(f'label_{label}_negative.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for item in negative_representatives:
+                writer.writerow([item])
+
+        print()
+
+#%%
+generate_datasets()
